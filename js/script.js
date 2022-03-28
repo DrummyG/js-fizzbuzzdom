9fa8bff5067651d80403d53c1072a1acd20abbb1
DrummyG <balestrinodavide@gmail.com>
2022-03-28 16:33:10 +0200

    bonus completo

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
-const cont = document.getElementById('container')
+const cont = document.querySelector('.riga')
 
 for (let i = 1; i <= 100; i++){
     if(i % 15 == 0) {
-        cont.innerHTML += `<p class="big">${i} FizzBuzz</p>`
+        cont.innerHTML += `<div class="square big">FizzBuzz</div>`
     } else if(i % 5 == 0) {
-        cont.innerHTML +=`<p class="medium">${i} Buzz</p>`
+        cont.innerHTML +=`<div class="square medium">Buzz</div>`
     } else if(i % 3 == 0) {
-        cont.innerHTML +=`<p class="small">${i} Fizz</p>`
+        cont.innerHTML +=`<div class="square smol">Fizz</div>`
+    } else{
+        cont.innerHTML +=`<div class="square numb">${i}</div>`
     }
 }
 
